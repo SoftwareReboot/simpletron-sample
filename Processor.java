46faa14586edcb2592217063a7f238b84fcefaa7
pspspspsps <eatdeesnutts3@gmail.com>
2025-09-10 15:48:51 +0800

    Add files via upload

---
+++ Processor.java
@@ -0,0 +1,21 @@
+public class Processor {
+   public Processor() {
+      
+   }
+   
+   public static void performOperation(int operation) {
+      switch (operation) {
+      
+      }
+   }
+   
+   public void readAdd(int address) {
+      
+   }
+   
+   public String write(int address){
+   
+   }
+   
+   public String loadM
+}
