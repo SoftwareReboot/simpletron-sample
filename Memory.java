384b341e6fe9ef10eefca3aadd1f74dbd077184a
Jeb <eatdeesnutts3@gmail.com>
2025-09-15 00:23:47 +0800

    added getter methods for memory addresses, current accumulators, and process counter

--- Memory.java
+++ Memory.java
@@ -1,13 +1,8 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
-
 public class Memory {
-   String[] addresses;
-   String currAcc;
-   int currCounter;
-   
-   
+   private static String[] addresses;
+   private static String currAcc;
+   private static int currCounter;
+      
    public Memory(int size) {
       addresses = new String[size];
       currAcc = new String("+0000");
@@ -24,65 +19,16 @@ public class Memory {
    public String getItem (int index) {
       return addresses[index];  
    }
-   
-   public void update() {
-      // load registers
-      System.out.println("REGISTERS:");
-      System.out.printf("accumulator:%11s%5s%n", " ", currAcc);
-      System.out.printf("programCounter:%11s%02d%n", " ", currCounter);
-      System.out.printf("instructionRegister:%3s%5s%n", " ", currAcc);
-      System.out.printf("operationCode:%12s%02d%n", " ", currCounter);
-      System.out.printf("operand:%18s%02d%n%n", " ", currCounter);
-      
-      // load memory addresses   
-      System.out.println("MEMORY:");
-      System.out.printf("%-6s", " ");
-      for (int i = 0; i < 10; i++) {
-         System.out.printf("%5d%4s", i, " ");
-      }
-      System.out.println();
-            
-      for (int i = 0; i < 100; i++) {
-         if (i % 10 == 0) {
-            System.out.printf("%02d", i);
-            System.out.printf("%4s", " ");
-         }
-         if (addresses[i].startsWith("-"))
-            System.out.print(addresses[i]);
-         else   
-            System.out.print("+" + addresses[i]);
-         System.out.printf("%4s", " ");
-         if (i % 10 == 9)
-            System.out.println();
-      }
+
+   public static String[] getAdds() {
+      return addresses;
    }
    
-   public static void main(String ...args) throws FileNotFoundException {
-      Memory mem = new Memory(100);
-      File file = new File("test.sml");
-      Scanner scan = new Scanner(file);
-      try {
-         while (scan.hasNextLine()) {
-            String line = scan.nextLine();
-            if (!line.isEmpty()) {
-               String[] tokens = line.trim().split("\\s+"); 
-               int index = Integer.parseInt(tokens[0]);
-               String value = tokens[1];
-               
-               mem.addItem(index, value);
-               
-               for (int i = 0; i < tokens.length; i++) {
-                  tokens[i] = null; 
-               }
-            }
-            else if (line.isEmpty() && scan.hasNextLine())
-               continue;
-            else 
-               break;
-         } 
-         mem.update();
-      } catch (Exception e) {
-         System.out.print(e.getMessage());
-      }
+   public static String getCurrAcc() {
+      return currAcc;
+   }
+
+   public static int getCurrCounter() {
+      return currCounter;
    }
 }
